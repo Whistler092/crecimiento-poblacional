d5374228f41858de28a3c91f5a24345be684768c
Ramiro Andrés <iamramiroo@gmail.com>
2019-05-18 15:58:56 -0500

    Updates

---
+++ precache-manifest.1d5e77d41bfc7bd082bb55d08b5c1f20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4093c05abcc6a1d4b713f092048f09",
+    "url": "/crecimiento-poblacional/index.html"
+  },
+  {
+    "revision": "a199a751cd85a664b569",
+    "url": "/crecimiento-poblacional/static/css/main.68a98085.chunk.css"
+  },
+  {
+    "revision": "a95ab72402f367b2ca64",
+    "url": "/crecimiento-poblacional/static/js/2.1c83d60f.chunk.js"
+  },
+  {
+    "revision": "a199a751cd85a664b569",
+    "url": "/crecimiento-poblacional/static/js/main.057b69a4.chunk.js"
+  },
+  {
+    "revision": "c7ecb9250089ce828d9e",
+    "url": "/crecimiento-poblacional/static/js/runtime~main.31bb0aaf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ff84dc3b598e9115bca100929919b9b.js"
+  "/crecimiento-poblacional/precache-manifest.1d5e77d41bfc7bd082bb55d08b5c1f20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crecimiento-poblacional/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
